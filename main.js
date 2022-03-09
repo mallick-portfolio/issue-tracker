c878ce50dcb482f76d1da164bfaee67c6d4207a8
Tamal Mallick <tamal1714019@gmail.com>
2022-03-09 15:33:10 +0600

    update error

--- main.js
+++ main.js
@@ -29,8 +29,11 @@ const closeIssue = (id) => {
     (issue) => issue.id.toString() === id.toString()
   );
   currentIssue.status = "Closed";
+
   localStorage.setItem("issues", JSON.stringify(issues));
   fetchIssues();
+  const title = document.getElementById("title");
+  title.style.textDecoration = "line-through";
 };
 
 const deleteIssue = (id, item) => {
@@ -57,7 +60,7 @@ const fetchIssues = () => {
     <div class="well">
       <h6>Issue ID: ${id} </h6>
       <p><span class="label label-info"> ${status} </span></p>
-      <h3> ${description} </h3>
+      <h3 id="title"> ${description} </h3>
       <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
       <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
       <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
